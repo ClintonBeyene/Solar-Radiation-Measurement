8e5ad53a130dbf9e897890393e33edd38d33b447
Clinton Beyene <clintonbeyene52@gmail.com>
2024-08-23 01:09:09 +0300

    all this files are modified and changed to new file

--- notebooks/mai.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-from scipy import stats 
-import matplotlib.pyplot as mpl
-
-def fetch_data(data):
-    dataframe =pd.read_csv(data)
-    return dataframe
